1a7289eada7d1c1cd5bff34560c8a9eaffa4c87d
Vasli Saidmuradov <vasli.said@gmail.com>
2021-12-26 13:13:34 +0600

    feat: day #4 - Is An Anagram (Easy). One more solution.

--- day-004.js
+++ day-004.js
@@ -45,3 +45,14 @@ function isAnagram(str1, str2) {
 
   return true;
 }
+
+// #3
+function isAnagram(str1, str2) {
+  let a = str1.toLowerCase();
+  let b = str2.toLowerCase();
+
+  a = a.split("").sort().join("");
+  b = b.split("").sort().join("");
+
+  return a === b;
+}
